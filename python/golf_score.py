16f10b91291a817730d87ef9358e50ac31b40c7a
hirai <whiskey.gokugoku@gmail.com>
2025-08-30 14:47:36 +0900

    Completed

--- python/golf_score.py
+++ python/golf_score.py
@@ -1,11 +1,44 @@
 # ゴルフスコア判定プログラム
 
-# ゴルフの規定打数(X)を受け取りリストに格納
-pars = list(map(int, input().split(",")))
-# プレイヤーの打数(Y)を受け取りリストに格納
-strokes = list(map(int, input().split(",")))
+# 入力された規定打数str(X)を,で分割してリスト:parsに格納
+# pars = list(map(int, input().split(","))
+# # 入力された実際の打数str(Y)を,で分割してリスト:strokesに格納
+# strokes = list(map(int, input().split(","))
+
+# 条件:(X は3 ≤ X ≤ 5,1 ≤ Y)以外の数値を入れられたらinput文を再表示するようにする
+# もし条件1,2,3が満たされない場合、再入力を要求する
+# 条件1：parsに3より下の数字and5より大きい数字不可
+# 条件2:strokesに1より小さい数字は不可
+# 条件3:pars,strokesに数字以外は不可
+limit = {"l1": 1, "l2": 3, "l3": 5}
+
+while True:
+    pars = input().split(",")
+    strokes = input().split(",")
+    # for文で回して数時間か否かを判断する
+    if not all(p.isdigit() for p in pars):
+        print("規定打数は数字で入力してください")
+        continue
+
+    if not all(s.isdigit() for s in strokes):
+        print("打数は数字で入力してください")
+        continue
+    # str型の数字をintに変換してリストに格納
+    pars = list(map(int, pars))
+    strokes = list(map(int, strokes))
+    # for文で回して規定打数,打数の数値が正しいかを判断する
+    if not all(limit["l2"] <= p <= limit["l3"] for p in pars):
+        print(f"規定打数に{"l2"}~{"l3"}以外の値を入れることはできません")
+        continue
+
+    if not all(s >= limit["l1"] for s in strokes):
+        print(f"打数には{limit["l1"]}より小さい値を入れることはできません")
+        continue
+
+    break
 
 # for文を使ってX,Yを回してスコアを判定していく
+# 取り出しやすいようにdictionary:relative_scoreにまとめる
 relative_scores = {
     "Par": "パー",
     "Birdie": "バーディ",
@@ -15,28 +48,32 @@ relative_scores = {
     "Ace": "ホールインワン",
     "Bogey": "ボギー",
 }
+
+# スコアを格納するlist:scoresを用意する
 scores = []
 
-for x, y in zip(pars, strokes):
-    if y - x == 0:
+# スコア判定条件
+# アルバトロスとコンドルとホールインワンの条件を整理
+
+for p, s in zip(pars, strokes):
+    if p == s:
         scores.append(relative_scores["Par"])
-    elif y - x == -1:
+    elif p - s == 1:
         scores.append(relative_scores["Birdie"])
-    elif y - x == -2:
+    elif p - s == 2 and s != 1:
         scores.append(relative_scores["Eagle"])
-    elif y == 5 and y == 2:
+    elif p == 5 and s == 2:
         scores.append(relative_scores["Albatross"])
-    elif y == 2 and y == 5:
+    elif p - s == 4 and p == 5:
         scores.append(relative_scores["Condor"])
-    elif y == 1:
+    elif s == 1:
         scores.append(relative_scores["Ace"])
-    elif y - x == 1:
+    elif p - s == -1:
         scores.append(relative_scores["Bogey"])
     else:
-        scores.append(f"{y - x}ボギー")
+        scores.append(f"{s - p}{relative_scores["Bogey"]}")
+
+
 # スコアのリストを出力
 result = ",".join(scores)
 print(result)
-
-
-# X-Yの形式でスコアを出力する
