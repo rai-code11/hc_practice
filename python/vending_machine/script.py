a407b5ebeba8e98778e35e975aedf30555289726
hirai <whiskey.gokugoku@gmail.com>
2025-09-08 13:34:11 +0900

    change due to list revisions

--- python/vending_machine/script.py
+++ python/vending_machine/script.py
@@ -1,6 +1,6 @@
-from .vending_machine import VM
+from .vending_machine import VendingMachine
 
-vm = VM()
+vm = VendingMachine()
 vm.suica1.charge(20000)
 result = vm.manage_purchase("pepsi", 4)
 print(result)
--- python/vending_machine/test.py
+++ python/vending_machine/test.py
@@ -1,27 +1,23 @@
 # Suicaの設計図
-# 属性：name,deposit,balance
-# メソッド：金額をチャージする,残高を表示する
-
-
+# 属性：name: str,balance: int
 class Suica:
     # 初期値の設定
-    def __init__(self, user_id, balance):
+    def __init__(self, user_id, balance=500):
         self.user_id = user_id
-        self.__balance = balance
-        deposit = 500
-        self.__balance += deposit
+        self._balance = balance
+        # deposit = 500
+        # self.balance += deposit
 
-    # VMクラスからbalanceを呼べるようにするためにgetterを設定する
+    # balanceに外部から不適切な編集をできないようにする
     @property
     def balance(self):
-        return self.__balance
+        return self._balance
 
-    # VMクラスから変更できるようにするためにsetterを設定する
     @balance.setter
     def balance(self, num):
         if num < 0:
             raise ValueError("残高にマイナスの数値を入れることはできません")
-        self.__balance = num
+        self._balance = num
 
     # 金額をチャージするメソッド
     def charge(self, charge_amount):
@@ -38,114 +34,125 @@ class Suica:
                 f"{charge_limit}円未満の金額をチャージすることはできません"
             )
         else:
-            self.__balance += charge_amount
+            self.balance += charge_amount
             return f"{charge_amount}円チャージしました\n{self.show_balance()}"
 
     # 残高を表示するメソッド
     def show_balance(self):
-        return self.__balance
+        return self.balance
 
 
 # juiceクラスからペプシインスタンスを作っていくという感じ
-# 属性:name,price
+# 属性:name: str,price: int
 class Juice:
     def __init__(self, name, price):
         self.name = name
         self.price = price
 
+    # priceに外部から不適切な編集をできないよ不適切な編集をできないようにする
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, num):
+        if not isinstance(num, int):
+            raise TypeError("価格は整数で入力してください")
+
+        if num < 0:
+            raise ValueError("価格にマイナスの値を入れることはできません")
+        self._price = num
+
 
 # VendingMachineクラス
-class VM:
+# 属性：stock: Dict[Juice, int],sales_amount: int
+class VendingMachine:
     def __init__(self):
-        # 1種類1インスタンスでドリンクを作成してリスト化する
-        self.pepsi_l = [Juice("pepsi", 150) for _ in range(5)]
-        self.monster_l = [Juice("monster", 230) for _ in range(5)]
-        self.irohasu_l = [Juice("irohasu", 120) for _ in range(5)]
-        # 取り出しやすいようにdict:self.drinksに格納
-        self.drinks = {
-            "pepsi": self.pepsi_l,
-            "monster": self.monster_l,
-            "irohasu": self.irohasu_l,
+        self.stock = {
+            Juice("pepsi", 150): 5,
+            Juice("monster", 230): 5,
+            Juice("irohasu", 120): 5,
         }
 
-        # Suicaインスタンスの情報を格納しておく
-        self.suica1 = Suica(1, 0)
-        self.suicas = {
-            "suica1": {"item": self.suica1},
-        }
-
-        self.__sales_amount = 0
-
-        # 在庫を補充のためのリスト作成
-        # 初期の在庫数を格納するリスト
-        self.base_stock_num = {}
-        for name, l in self.drinks.items():
-            self.base_stock_num[name] = len(l)
-
-        # 在庫補充の際にインスタンスを作るための商品価格表
-        self.price_list = {"pepsi": 150, "monster": 230, "irohasu": 120}
+        self._sales_amount = 0
 
     # 在庫数を取得するメソッド
     def get_stock(self, item_name):
         self.item_name = item_name
-        self.stock_num = len(self.drinks[self.item_name])
-        return self.stock_num
+        for juice, qty in self.stock.items():
+            if item_name == juice.name:
+                return qty
 
     # 購入できるか判断するメソッド
     # 購入金額よりも残高の方が大きいand購入個数より在庫が多い
     # purchase_qty:購入個数
-    def can_purchase(self, item_name, purchase_qty):
-        self.item_name = item_name
+    # ユーザーが作成したsuicaインスタンスを引数のとる
+    def can_purchase(self, suica, item_name, purchase_qty):
+        self.suica = suica
         self.purchase_qty = purchase_qty
+
         # 購入金額 = 値段 × 購入個数
         # item_nameの変数で取得できるようにする
-        price = self.price_list[item_name]
+        price = self.get_price(item_name)
         self.purchase_amount = price * self.purchase_qty
-        # Suicaクラスからインスタンスを作成してそのbalanceを持ってこないとけない？
+
         # if文は真偽値なのでそのまま返す
         return (
-            self.purchase_amount <= self.suica1.balance
-            and self.purchase_qty <= self.get_stock(self.item_name)
+            self.purchase_amount <= self.suica.balance
+            and self.purchase_qty <= self.get_stock(item_name)
         )
+
+    # 購入判断のための商品の価格を取ってくるメソッド
+    def get_price(self, item_name):
+        for juice in self.stock:
+            if juice.name == item_name:
+                return juice.price
 
     # 購入処理をまとめる
     # 購入処理：①在庫を減らす②売上金額を増やす③Suicaのチャージ残高を減らす
     # もしmanage_purchaseがTrueだったら購入処理を実行する
-    def manage_purchase(self, item_name, purchase_qty):
-        self.item_name = item_name
-        self.purchase_qty = purchase_qty
-        if self.can_purchase(item_name, purchase_qty):
-            reduce_after = self.reduce_stock(item_name, purchase_qty)
+    def manage_purchase(self, suica, item_name, purchase_qty):
+
+        if self.can_purchase(suica, item_name, purchase_qty):
+            reduce_after = self.reduce_stock(item_name)
             amount = self.calculate_sales_amount()
-            balance_after = self.reduce_balance()
+            balance_after = self.reduce_balance(suica)
 
             return reduce_after, amount, balance_after
         else:
             raise ValueError(f"在庫がないかチャージ金額が足りません")
 
+    @property
+    def purchase_qty(self):
+        return self._purchase_qty
+
+    @purchase_qty.setter
+    def purchase_qty(self, num):
+        if not isinstance(num, int):
+            raise TypeError("値は整数を入力してください")
+        if num < 0:
+            raise ValueError("マイナスの値を入れることはできません")
+        self._purchase_qty = num
+
     # 在庫を減らすメソッド
-    def reduce_stock(self, item_name, purchase_qty):
-        self.item_name = item_name
-        self.purchase_qty = purchase_qty
+    def reduce_stock(self, item_name):
         # 在庫数 = 在庫数 - 購入数
-        # list:drinksからitem_nameを指定してpurchase_qty回リストから削除を繰り返す
-        # 変数stockにitem_name対象インスタンスのリストを格納
-        stock = self.drinks[item_name]
-        for _ in range(purchase_qty):
-            stock.pop()
-        return len(stock)
+        for juice in self.stock:
+            if item_name == juice.name:
+                self.stock[juice] -= self.purchase_qty
+                return self.stock[juice]
 
     # 売上金額を増やしていくメソッド
     def calculate_sales_amount(self):
         # 売上金額 = 売上金額 + 購入金額
-        self.__sales_amount += self.purchase_amount
-        return self.__sales_amount
+        self._sales_amount += self.purchase_amount
+        return self._sales_amount
 
     # Suicaのチャージ残高を減らすメソッド
-    def reduce_balance(self):
+    def reduce_balance(self, suica):
         # 残高 = 残高 - 購入金額
-        self.suica1.balance -= self.purchase_amount
-        return self.suica1.balance
+        suica.balance -= self.purchase_amount
+        return suica.balance
 
     # 購入可能なドリンクのリストを取得するメソッド
     def get_purchase_list(self):
@@ -154,31 +161,36 @@ class VM:
 
         # 省略前
         # purchase_qty_list = []
-        # for k, v in self.drinks.items():
-        #     if len(v) > 0:
-        #         purchase_qty_list.append(k)
+        # for juice, qty in self.stock.items():
+        #     if qty > 0:
+        #         purchase_qty_list.append(juice.name)
         # return purchase_qty_list
 
-        # 省略後(リスト内包表記)
-        purchase_qty_list = [k for k, v in self.drinks.items() if len(v) > 0]
+        # 省略後
+        purchase_qty_list = [juice.name for juice, qty in self.stock.items() if qty > 0]
         return purchase_qty_list
 
     # 在庫を追加するメソッド
-    # 基準在庫数をを設定して補充メソッドを呼び出した時に基準在庫数になる
-    # 基準在庫数はget_stock(item_name)で取得する
+    # 購入された分、在庫数に足してその値をdict:stockに代入する
     def add_stock(self, item_name):
-        base = self.base_stock_num[item_name]
-        current = self.get_stock(item_name)
-        diff = base - current
-
-        for _ in range(diff):
-            self.drinks[item_name].append(Juice(item_name, self.price_list[item_name]))
-        return len(self.drinks[item_name])
+        for juice, qty in self.stock.items():
+            if item_name == juice.name:
+                self.stock[juice] = qty + self.purchase_qty
+        return self.stock[juice]
 
 
-vm = VM()
-vm.suica1.charge(20000)
-result = vm.manage_purchase("pepsi", 4)
-print(result)
+suica1 = Suica("user1")
+show_balance = suica1.show_balance()
+print(show_balance)
+charge = suica1.charge(1000)
+print(charge)
+
+vm = VendingMachine()
+manage_purchase = vm.manage_purchase(suica1, "pepsi", 5)
+print(manage_purchase)
+purchsae_l = vm.get_purchase_list()
+print(purchsae_l)
 result_stock = vm.add_stock("pepsi")
 print(result_stock)
+purchsae_l = vm.get_purchase_list()
+print(purchsae_l)
